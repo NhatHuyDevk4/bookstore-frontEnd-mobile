4ae18751d90d42d85b589910ab73864c53d738e0
Lê Nhật Huy <99423466+NhatHuyDevk4@users.noreply.github.com>
2025-05-06 20:31:54 +0700

    Add tabs navigate (#4)

--- store/authStore.js
+++ store/authStore.js
@@ -12,7 +12,7 @@ export const useAuthStore = create((set) => ({
         set({ isLoading: true });
 
         try {
-            const response = await fetch('http://localhost:3000/api/auth/register', {
+            const response = await fetch('https://bookstore-backend-mobile.onrender.com/api/auth/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -39,5 +39,56 @@ export const useAuthStore = create((set) => ({
         } finally {
             set({ isLoading: false });
         }
+    },
+
+    login: async (email, password) => {
+        set({ isLoading: true });
+
+        try {
+            const response = await fetch('https://bookstore-backend-mobile.onrender.com/api/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email, password }),
+            });
+            console.log('Response:', response); // Log the response object
+
+            const data = await response.json();
+
+            if (!response.ok) {
+                throw new Error(data.message || 'Something went wrong!');
+            }
+
+            await AsyncStorage.setItem('user', JSON.stringify(data.user));
+            await AsyncStorage.setItem('token', data.token);
+
+            set({ user: data.user, token: data.token, isLoading: false });
+
+            return { success: true };
+        } catch (error) {
+            console.error('Error during login:', error.message);
+            return { success: false, error: error.message };
+        } finally {
+            set({ isLoading: false });
+        }
+    },
+
+    checkAuth: async () => {
+        try {
+            const token = await AsyncStorage.getItem('token');
+            const userJson = await AsyncStorage.getItem('user');
+            const user = userJson ? JSON.parse(userJson) : null;
+
+            set({ token, user });
+        } catch (error) {
+            console.error('Error retrieving data from AsyncStorage:', error);
+        }
+    },
+
+    logout: async () => {
+        await AsyncStorage.removeItem('token');
+        await AsyncStorage.removeItem('user');
+        set({ token: null, user: null });
     },
 }));
